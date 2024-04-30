bb60d701183427e1e86c16f88e6bf9c3c1dd202d
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-30 19:12:55 -0400

    Got the back end displaying the data from the front end with notes taken in the db.json. I rolled back the uuid to an older version and the updated version was not compatible, and it was fixed.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,10 @@
 const app = require('express').Router();
 const dataBase = require('../db/store')
 app.get('/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    dataBase.getNotes()
+    .then((note)=>{
+        //grabs the class of store and turn them into json
+        res.json(note)
+    })
 });
 module.exports = app;
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 
 // Helper method for generating unique ids
-//const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = 3001;
 
 const app = express();
-//Middleware
+//Middleware working as the center piece of the server side
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 //Gathering data
 app.use('/', htmlRoutes);
-//app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
