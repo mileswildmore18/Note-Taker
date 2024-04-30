9d28b1c38fded6448d1cf9e1b4947aae3c50a6d6
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-30 19:48:15 -0400

    Fixed the issue with when the note is added, it appears on the same page on the website and is permanently there even when it is closed, and is still there when the website is reopened. Also added a delete function to when the user clicks the trash can icon, the note gets removed permanently even when the webpage is revisited.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,9 +3,24 @@ const app = require('express').Router();
 const dataBase = require('../db/store')
 app.get('/notes', (req, res) => {
     dataBase.getNotes()
+        .then((note) => {
+            //grabs the class of store and turn them into json
+            res.json(note)
+        })
+});
+app.post('/notes', (req, res) => {
+    dataBase.addNote(req.body)
+        // Destructuring assignment for the items in req.body
     .then((note)=>{
-        //grabs the class of store and turn them into json
         res.json(note)
     })
-});
+
+})//Always a different paramater, a changing variable
+app.delete('/notes/:id',(req, res) => {
+    dataBase.removeNote(req.params.id)//responds with the note, 
+    //deletes the note
+    .then((note)=>{
+        res.json(note)
+    })
+})
 module.exports = app;
